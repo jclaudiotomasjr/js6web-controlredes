add0c5f4d0932e6bbb21b7785371dded684c3725
jtomas <jclaudiotomas@gmail.com>
2023-03-30 01:17:51 -0300

    first commit api

---
+++ src/test/java/js6web/controlredes/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package js6web.controlredes.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
